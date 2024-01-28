e46624831fee571f28addc65e2e166ea5d19a1f3
Nishant Khalate <nishantkhalate10@gmail.com>
2024-01-28 16:43:32 +0530

    Dukkar1.0 apk build

--- Pixel Escape 2D/Assets/Scripts/GameManager.cs
+++ Pixel Escape 2D/Assets/Scripts/GameManager.cs
@@ -15,18 +15,10 @@ public class GameManager : MonoBehaviour
     public GameObject tapToStartText;
     public TextMeshProUGUI scoreText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-            
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && !isGameStarted)
         {
-            //tapToStartText.SetActive(false);
             uiManager.modifyActiveStatusOfElements(tapToStartText, false);
             StartSpawning();
             isGameStarted = true;
